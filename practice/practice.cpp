945d43bd40e4b9ca4bd58619a0caacfc14c51761
YunoGasasi9862 <m.bilal9862@gmail.com>
2020-11-14 21:24:01 +0500

    Add project files.

---
+++ practice/practice.cpp
@@ -0,0 +1,19 @@
+
+
+#include <iostream>
+
+int main()
+{
+    int age=11;
+    std::cout << "Hello World!\n";
+
+    std::cout << age;
+    std::cout << square(2);
+
+}
+double square(int x) {
+
+    return x * x;
+
+
+}
